bb5d685eff67edbdeb884c5abf7acc0974f2d8e0
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2022-11-13 20:57:12 +0530

    feat: Added find minimum number function.

--- main.c
+++ main.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int num1, num2, num3, maxNum;
+int num1, num2, num3, maxNum, minNum;
 
     int max(){
         if (num1 > num2 && num1 > num3)
@@ -15,10 +13,23 @@ int main()
             printf("The maximum number is : %d", maxNum);
         }
 
+    int min(){
+        if (num1 < num2 && num1 < num3)
+            minNum = num1;
+        if (num2 < num1 && num2 < num3)
+            minNum = num2;
+        if (num3 < num1 && num3 < num2)
+            minNum = num3;
+            printf("\nThe minimum number is : %d", minNum);
+        }
+
+int main()
+{
     printf("Enter three numbers (ex:4 8 2): ");
     scanf("%d %d %d", &num1,&num2, &num3);
 
     max(num1, num2, num3);
+    min(num1, num2, num3);
 
     return 0;
 }
